075425f0241b2dfff030805a090dafeb62d3a178
Eliabr Rodrigues <eliabe859@gmail.com>
2023-12-28 13:39:31 -0300

    closes #3 finalizado a soma

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function calc(){
-    
+    var num1 = parseFloat(document.getElementById("primeiroNumero").value)
+    var num2 = parseFloat(document.getElementById("segundoNumero").value)
+
+    var res = num1 + num2;
+
+    document.getElementById("display").value = res;
 }
