28124e925b9d992ca7ba33528058c3110eba1d04
Eliabr Rodrigues <eliabe859@gmail.com>
2023-12-29 11:51:05 -0300

    closes #4 Finalizado as operaçoes

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-function calc(){
+function calc(op){
+    var ope = document.getElementById("operacao").value
     var num1 = parseFloat(document.getElementById("primeiroNumero").value)
     var num2 = parseFloat(document.getElementById("segundoNumero").value)
 
-    var res = num1 + num2;
+     
+    console.log(ope);
+    
+    switch (op) {
+        case '+':
+            res = num1+num2
+            break;
+
+        case '-':
+            res = num1-num2"
+            break;
+
+        case '*':
+            res = num1*num2
+            break;
+
+        case '/':
+            res = num1/num2
+            break;
+    }
+
+    
+
+
 
     document.getElementById("display").value = res;
+
+
+
+
 }
